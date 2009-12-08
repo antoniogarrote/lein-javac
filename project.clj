7d45941b2ad1c33d1fae2d2f2f559b72ec9dc109
Antonio Garrote <antoniogarrote@gmail.com>
2009-12-08 22:09:53 +0100

    Version 0.0.1

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-javac "0.0.1-SNAPSHOT"
+  :description "javac plugin for leiningen"
+  :namespaces [other-lancet leiningen.javac]
+  :dev-dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                     [leiningen "1.0.0"]])
---
+++ src/leiningen/javac.clj
@@ -0,0 +1,25 @@
+;;; javac plugin
+
+
+(ns leiningen.javac
+  (:use [leiningen.compile])
+  (:refer-clojure :exclude [compile]))
+
+(defn lib-path [project]
+  (apply make-path
+         (:source-path project)
+         (:test-path project)
+         (:compile-path project)
+         (:resources-path project)
+         (find-lib-jars project)))
+
+(defn compile-java [project]
+  (let [project-root (:source-path project)]
+    (other-lancet/javac {:srcdir (str project-root "/src")
+                         :destdir (str project-root "/classes")
+                         :includejavaruntime "yes"
+                         :classpath (lib-path project)
+                         :debug (or (str (:javac-debug project))
+                                    "false")
+                         :target (or (str (:javac-target project))
+                                     "1.5")})))
