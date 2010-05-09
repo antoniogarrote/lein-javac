fcc00dfe7c96354491ebba6b72edab2779ee433c
Caspar Florian Ebeling <florian.ebeling@nugg.ad>
2010-05-09 18:08:19 +0200

    project.clj: change deps version to existing stable ones.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-javac "0.0.3-SNAPSHOT"
   :description "javac plugin for leiningen"
   :namespaces [lancet leiningen.compile-java]
-  :dev-dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dev-dependencies [[org.clojure/clojure "1.1.0"]
+                     [org.clojure/clojure-contrib "1.1.0"]
                      [leiningen "1.0.0"]])
