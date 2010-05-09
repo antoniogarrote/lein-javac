12cad495f24f983c73ab1e5fda33b13d94db9fc9
Caspar Florian Ebeling <florian.ebeling@nugg.ad>
2010-05-09 18:06:58 +0200

    project.clj: remove other_lancet reference. use lancet instead.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-javac "0.0.3-SNAPSHOT"
   :description "javac plugin for leiningen"
-  :namespaces [other-lancet leiningen.compile-java]
+  :namespaces [lancet leiningen.compile-java]
   :dev-dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                      [leiningen "1.0.0"]])
