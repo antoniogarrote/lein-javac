1a34ec78ab1a8134308728b3f8714a0d5cabb11b
Caspar Florian Ebeling <florian.ebeling@nugg.ad>
2010-05-10 14:21:24 +0200

    leiningen.compile-java and leininge.compile-java-tests: refactorings.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject lein-javac "0.0.3-SNAPSHOT"
   :description "javac plugin for leiningen"
-  :namespaces [lancet leiningen.compile-java]
-  :dev-dependencies [[org.clojure/clojure "1.1.0"]
+  :namespaces [leiningen.compile-java
+	       leiningen.compile-java-tests]
+  :dependencies [[org.clojure/clojure "1.1.0"]
                      [org.clojure/clojure-contrib "1.1.0"]
-                     [leiningen "1.0.0"]])
+                     [leiningen "1.1.0"]])
--- src/leiningen/compile_java.clj
+++ src/leiningen/compile_java.clj
@@ -1,16 +1,12 @@
 ;;; javac plugin
 
-(ns lancet
-  (:require [lancet]))
-
-(defmethod coerce [org.apache.tools.ant.types.Path String] [_ str]
-  (new org.apache.tools.ant.types.Path ant-project str))
-
-
 (ns leiningen.compile-java
-  (:require [lancet])
-  (:use [leiningen.compile])
+  (:require lancet)
+  (:use leiningen.compile)
   (:refer-clojure :exclude [compile]))
+
+(defmethod lancet/coerce [org.apache.tools.ant.types.Path String] [_ str]
+  (new org.apache.tools.ant.types.Path lancet/ant-project str))
 
 (defn lib-path [project]
   (apply make-path
@@ -20,17 +16,20 @@
          (:resources-path project)
          (find-lib-jars project)))
 
+(defn javac-defaults [project]
+  {:includejavaruntime "yes"
+   :debug (or (:javac-debug project)
+	      "false")
+   :target (or (:javac-target project)
+	       "1.5")
+   :fork (or (:javac-fork project)
+	     "true")})
+
 (defn compile-java [project]
   (let [project-root (:root project)]
-    (lancet/javac {:srcdir (or (:java-source-path project)
+    (lancet/javac (merge (javac-defaults project)
+			 {:srcdir (or (:java-source-path project)
                                      (:source-path project))
                          :destdir (or (:compile-path project)
 				      (str project-root "/classes"))
-                         :includejavaruntime "yes"
-                         :classpath (lib-path project)
-                         :debug (or (:javac-debug project)
-                                    "false")
-                         :target (or (:javac-target project)
-                                     "1.5")
-                         :fork (or (:javac-fork project)
-                                   "true")})))
+                         :classpath (lib-path project)}))))
