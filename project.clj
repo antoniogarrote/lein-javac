2dfc9123101edce140f25248daa56c7b9b665da4
Roman Scherer <roman.scherer@nugg.ad>
2010-07-21 12:34:52 +0200

    Added swank-clojure to dev dependencies.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "javac plugin for leiningen"
   :dependencies [[org.clojure/clojure "1.1.0"]
 		 [org.clojure/clojure-contrib "1.1.0"]
-		 [leiningen "1.1.0"]])
+		 [leiningen "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
