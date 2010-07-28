c4808b949b600b801765f90d7cebe3e30e2f292a
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2010-07-28 23:03:02 +0200

    project.clj: remove dependency declaration to leiningen, this is implicit for plugins.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject lein-javac "0.0.3-SNAPSHOT"
   :description "javac plugin for leiningen"
   :dependencies [[org.clojure/clojure "1.1.0"]
-		 [org.clojure/clojure-contrib "1.1.0"]
-		 [leiningen "1.2.0"]]
+		 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
