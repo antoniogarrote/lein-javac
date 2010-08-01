fe13f938b2ce38da554304e0b842ba0d2152eee2
Roman Scherer <roman@burningswell.com>
2010-08-01 09:38:28 +0200

    Switched to ant-launcher 1.8.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-javac "1.2.1-SNAPSHOT"
   :description "Java compiler plugin for Leiningen."
-  :dependencies [[ant/ant-launcher "1.6.5"]
+  :dependencies [[org.apache.ant/ant-launcher "1.8.1"]
                  [org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[leiningen/leiningen "1.2.1-SNAPSHOT"]
