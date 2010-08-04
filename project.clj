ddae2b0f892a03f38309e4366cd98f1152e9b4c7
Roman Scherer <roman.scherer@nugg.ad>
2010-08-04 13:31:50 +0200

    Switched to leiningen 1.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[ant/ant-launcher "1.6.5"]
                  [org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
-  :dev-dependencies [[leiningen/leiningen "1.2.1-SNAPSHOT"]
+  :dev-dependencies [[leiningen/leiningen "1.3.0-SNAPSHOT"]
                      [robert/hooke "1.0.2"]
                      [swank-clojure "1.2.1"]])
