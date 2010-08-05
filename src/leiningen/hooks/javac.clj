ea49d8bffdb18764c9ae5a584ba712f9aa93da9d
Roman Scherer <roman.scherer@nugg.ad>
2010-08-05 09:52:32 +0200

    Added a clean hook for javac.

--- src/leiningen/hooks/javac.clj
+++ src/leiningen/hooks/javac.clj
@@ -1,10 +1,17 @@
 (ns leiningen.hooks.javac
-  (:require leiningen.compile)
-  (:use [leiningen.javac :only (javac)]
+  (:require leiningen.clean leiningen.compile)
+  (:use [clojure.contrib.io :only [file delete-file delete-file-recursively]]
+        [leiningen.javac :only (javac extract-compile-tasks)]
         robert.hooke))
 
-(defn compile-java-hook [task & args]
+(defn clean-javac-hook [task & args]
+  (apply task args)
+  (let [tasks (extract-compile-tasks (first args))]
+    (delete-file-recursively (:destdir task))))
+
+(defn compile-javac-hook [task & args]
   (apply javac args)
   (apply task args))
 
-(add-hook #'leiningen.compile/compile compile-java-hook)
+(add-hook #'leiningen.clean/clean clean-javac-hook)
+(add-hook #'leiningen.compile/compile compile-javac-hook)
