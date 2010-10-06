52eb5c94af83c49e970eaac7f601ea02b890b0be
Roman Scherer <roman.scherer@nugg.ad>
2010-10-06 15:43:30 +0200

    Upgraded to clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-javac "1.2.1-SNAPSHOT"
   :description "Java compiler plugin for Leiningen."
   :dependencies [[ant/ant-launcher "1.6.5"]
-                 [org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[leiningen/leiningen "1.3.0-SNAPSHOT"]
                      [robert/hooke "1.0.2"]
                      [swank-clojure "1.2.1"]])
