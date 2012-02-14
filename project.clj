fd177fd2bc2e7144e9fa5241b8f2ed08fe09cef9
Roman Scherer <roman.scherer@nugg.ad>
2012-02-14 17:41:51 +0100

    Removed clojure dependency.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject org.clojars.r0man/lein-javac "1.2.2-SNAPSHOT"
+(defproject org.clojars.r0man/lein-javac "1.2.3-SNAPSHOT"
   :description "Java compiler plugin for Leiningen."
   :dependencies [[ant/ant-launcher "1.6.5"]]
   :dev-dependencies [[leiningen/leiningen "1.6.2-SNAPSHOT"]
-                     [org.clojure/clojure "1.2.1"]
                      [robert/hooke "1.1.2"]])
