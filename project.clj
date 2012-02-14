81978374ae0b8a69bc85fac48c55efe7f8132892
Roman Scherer <roman.scherer@nugg.ad>
2012-02-14 17:50:38 +0100

    Depend on Leiningen 1.6.2.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.r0man/lein-javac "1.2.3-SNAPSHOT"
+(defproject org.clojars.r0man/lein-javac "1.2.4-SNAPSHOT"
   :description "Java compiler plugin for Leiningen."
   :dependencies [[ant/ant-launcher "1.6.5"]]
-  :dev-dependencies [[leiningen/leiningen "1.6.2-SNAPSHOT"]
+  :dev-dependencies [[leiningen/leiningen "1.6.2"]
                      [robert/hooke "1.1.2"]])
